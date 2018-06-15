66889dc25e9d17b05eff993135e872720e4d8902
Eduardo Lezcano Alvarez <eduardo.lezcano@be.atlascopco.com>
2018-06-15 13:22:38 +0200

    Fixing foodcritic

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,9 +9,9 @@ url = "#{node['jfrog']['cli']['url']['root']}/"
 if node['kernel']['name'] == 'linux'
   case node['kernel']['machine']
   when 'x86_64'
-    url += "#{node['jfrog']['cli']['url']['linux_amd64']}"
+    url += node['jfrog']['cli']['url']['linux_amd64']
   when 'i386'
-    url += "#{node['jfrog']['cli']['url']['linux_i386']}"
+    url += node['jfrog']['cli']['url']['linux_i386']
   else
     raise "Machine #{node['kernel']['machine']} not supported."
   end
