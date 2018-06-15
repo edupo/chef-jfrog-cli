f76d456fef5866b5f656c24aad70de3da25ac6b8
Eduardo Lezcano Alvarez <eduardo.lezcano@be.atlascopco.com>
2018-06-15 13:39:55 +0200

    Support for debian is also granted. Probably to many other platforms...

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,11 @@ maintainer_email 'github@eduardolezcano.com'
 license 'MIT'
 description 'Installs jfrog_cli'
 long_description 'Installs jfrog_cli'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/edupo/chef-jfrog-cli/issues'
 source_url 'https://github.com/edupo/chef-jfrog-cli'
 
 supports 'ubuntu'
+supports 'debian'
