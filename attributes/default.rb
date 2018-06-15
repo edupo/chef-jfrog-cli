ef911253b60b19df115b75c41a4b76e1e6715dad
Eduardo Lezcano Alvarez <eduardo.lezcano@be.atlascopco.com>
2018-06-15 13:19:34 +0200

    Recognizing linux machine type

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,13 @@
 default['jfrog']['cli']['bin'] = '/usr/bin/jfrog'
+
 default['jfrog']['cli']['url']['root'] = 
   'https://api.bintray.com/content/jfrog/jfrog-cli-go/$latest'
-default['jfrog']['cli']['url']['linux64'] = 
+
+default['jfrog']['cli']['url']['linux_amd64'] = 
   'jfrog-cli-linux-amd64/jfrog?bt_package=jfrog-cli-linux-amd64'
+default['jfrog']['cli']['url']['linux_i386'] = 
+  'jfrog-cli-linux-386/jfrog?bt_package=jfrog-cli-linux-386'
+default['jfrog']['cli']['url']['win'] = 
+  'jfrog-cli-windows-amd64/jfrog.exe?bt_package=jfrog-cli-windows-amd64'
+default['jfrog']['cli']['url']['mac'] = 
+  'jfrog-cli-mac-386/jfrog?bt_package=jfrog-cli-mac-386'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,18 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-url = "#{node['jfrog']['cli']['url']['root']}/#{node['jfrog']['cli']['url']['linux64']}"
+url = "#{node['jfrog']['cli']['url']['root']}/"
+
+if node['kernel']['name'] == 'linux'
+  case node['kernel']['machine']
+  when 'x86_64'
+    url += "#{node['jfrog']['cli']['url']['linux_amd64']}"
+  when 'i386'
+    url += "#{node['jfrog']['cli']['url']['linux_i386']}"
+  else
+    raise "Machine #{node['kernel']['machine']} not supported."
+  end
+end
 
 remote_file node['jfrog']['cli']['bin'] do
   source url
